14f41776cdbd22566a18ecf1635861c415bd274e
max75025 <max75025@gmail.com>
2017-10-29 19:00:26 +0200

    add pr1

---
+++ pr1/pr1/Form1.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace pr1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.сохранитьВРеестToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.извлечьИзРеестраToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.записатьВАвтозагрузкуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьИзАвтозагрузкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.textBox1.Location = new System.Drawing.Point(12, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(322, 39);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "7";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(10, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "4";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 47);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(10, 96);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 47);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "±";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 308);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(154, 47);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "0";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(91, 255);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 47);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "2";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(91, 202);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 47);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "5";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(91, 149);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 47);
+            this.button9.TabIndex = 7;
+            this.button9.Text = "8";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(91, 96);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 47);
+            this.button10.TabIndex = 6;
+            this.button10.Text = "C";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.button11.Location = new System.Drawing.Point(172, 308);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 47);
+            this.button11.TabIndex = 15;
+            this.button11.Text = ",";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(172, 255);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 47);
+            this.button12.TabIndex = 14;
+            this.button12.Text = "3";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(172, 202);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(75, 47);
+            this.button13.TabIndex = 13;
+            this.button13.Text = "6";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(172, 149);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(75, 47);
+            this.button14.TabIndex = 12;
+            this.button14.Text = "9";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(172, 96);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 47);
+            this.button15.TabIndex = 11;
+            this.button15.Text = "←";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(257, 308);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(75, 47);
+            this.button16.TabIndex = 20;
+            this.button16.Text = "=";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(257, 255);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(75, 47);
+            this.button17.TabIndex = 19;
+            this.button17.Text = "+";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(257, 202);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(75, 47);
+            this.button18.TabIndex = 18;
+            this.button18.Text = "-";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(257, 149);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(75, 47);
+            this.button19.TabIndex = 17;
+            this.button19.Text = "*";
+            this.button19.UseVisualStyleBackColor = true;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(257, 96);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(75, 47);
+            this.button20.TabIndex = 16;
+            this.button20.Text = "/";
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 21;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(344, 25);
+            this.toolStrip1.TabIndex = 22;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьВРеестToolStripMenuItem,
+            this.извлечьИзРеестраToolStripMenuItem,
+            this.записатьВАвтозагрузкуToolStripMenuItem,
+            this.удалитьИзАвтозагрузкиToolStripMenuItem});
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(69, 22);
+            this.toolStripDropDownButton1.Text = "функции";
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // сохранитьВРеестToolStripMenuItem
+            // 
+            this.сохранитьВРеестToolStripMenuItem.Name = "сохранитьВРеестToolStripMenuItem";
+            this.сохранитьВРеестToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.сохранитьВРеестToolStripMenuItem.Text = "сохранить в реест";
+            this.сохранитьВРеестToolStripMenuItem.Click += new System.EventHandler(this.сохранитьВРеестToolStripMenuItem_Click);
+            // 
+            // извлечьИзРеестраToolStripMenuItem
+            // 
+            this.извлечьИзРеестраToolStripMenuItem.Name = "извлечьИзРеестраToolStripMenuItem";
+            this.извлечьИзРеестраToolStripMenuItem.Size = new System.Drawing.Size(179, 22);
+            this.извлечьИзРеестраToolStripMenuItem.Text = "извлечь из реестра";
+            this.извлечьИзРеестраToolStripMenuItem.Click += new System.EventHandler(this.извлечьИзРеестраToolStripMenuItem_Click);
+            // 
+            // записатьВАвтозагрузкуToolStripMenuItem
+            // 
+            this.записатьВАвтозагрузкуToolStripMenuItem.Name = "записатьВАвтозагрузкуToolStripMenuItem";
+            this.записатьВАвтозагрузкуToolStripMenuItem.Size = new System.Drawing.Size(204, 22);
+            this.записатьВАвтозагрузкуToolStripMenuItem.Text = "записать в автозагрузку";
+            this.записатьВАвтозагрузкуToolStripMenuItem.Click += new System.EventHandler(this.записатьВАвтозагрузкуToolStripMenuItem_Click);
+            // 
+            // удалитьИзАвтозагрузкиToolStripMenuItem
+            // 
+            this.удалитьИзАвтозагрузкиToolStripMenuItem.Name = "удалитьИзАвтозагрузкиToolStripMenuItem";
+            this.удалитьИзАвтозагрузкиToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
+            this.удалитьИзАвтозагрузкиToolStripMenuItem.Text = "удалить из автозагрузки";
+            this.удалитьИзАвтозагрузкиToolStripMenuItem.Click += new System.EventHandler(this.удалитьИзАвтозагрузкиToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 369);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьВРеестToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem извлечьИзРеестраToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem записатьВАвтозагрузкуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьИзАвтозагрузкиToolStripMenuItem;
+    }
+}
+
